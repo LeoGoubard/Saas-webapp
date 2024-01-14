941521d80a22471854a000e797bf7ed6b00d981b
LEO GOUBARD <goubardleo@gmail.com>
2024-01-14 14:57:39 +0100

    :feat: crud for the banner of the Workspace, small error to fix, there is an undefined id when i navigate from a workspace to a File

--- src/components/quill-editor/quill-editor.tsx
+++ src/components/quill-editor/quill-editor.tsx
@@ -5,7 +5,7 @@ import { File, Folder, workspace } from '@/lib/supabase/supabase.types'
 import React, { useCallback, useMemo, useState } from 'react'
 import 'quill/dist/quill.snow.css';
 import { Button } from '../ui/button';
-import { deleteFile, deleteFolder, updateFile, updateFolder } from '@/lib/supabase/queries';
+import { deleteFile, deleteFolder, updateFile, updateFolder, updateWorkspace } from '@/lib/supabase/queries';
 import { usePathname } from 'next/navigation';
 import { Tooltip, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
@@ -13,6 +13,9 @@ import { TooltipContent } from '@radix-ui/react-tooltip';
 import { Badge } from '../ui/badge';
 import Image from 'next/image';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import EmojiPicker from '../global/emoji-picker';
+import { XCircleIcon } from 'lucide-react';
+import BannerUpload from '../baner-upload/banner-upload';
 
 interface QuillEditorProps {
   dirDetails: File | Folder | workspace;
@@ -44,6 +47,7 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ dirType, fileId, dirDetails }
   const supabase = createClientComponentClient()
   const { state, workspaceId, folderId, dispatch } = useAppState();
   const [quill, setQuill] = useState<any>(null)
+  const [deletingBanner, setDeletingBanner] = useState(false);
   const [collaborators, setCollaborators] = useState<{id: string; email: string; avatarUrl: string}[]>([
     { id: '1212', email: 'test@gmail.com', avatarUrl: '123123' }
   ])
@@ -98,6 +102,74 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ dirType, fileId, dirDetails }
       setQuill(q)
     }
   }, [])
+
+  const iconOnChange = async (icon: string) => {
+    if (!fileId) return;
+    if (dirType === 'workspace') {
+      dispatch({
+        type: 'UPDATE_WORKSPACE',
+        payload: { workspace: { iconId: icon }, workspaceId: fileId },
+      });
+      await updateWorkspace({ iconId: icon }, fileId);
+    }
+    if (dirType === 'folder') {
+      if (!workspaceId) return;
+      dispatch({
+        type: 'UPDATE_FOLDER',
+        payload: {
+          folder: { iconId: icon },
+          workspaceId,
+          folderId: fileId,
+        },
+      });
+      await updateFolder({ iconId: icon }, fileId);
+    }
+    if (dirType === 'file') {
+      if (!workspaceId || !folderId) return;
+
+      dispatch({
+        type: 'UPDATE_FILE',
+        payload: { file: { iconId: icon }, workspaceId, folderId, fileId },
+      });
+      await updateFile({ iconId: icon }, fileId);
+    }
+  };
+
+  const deleteBanner = async () => {
+    if (!fileId) return;
+    setDeletingBanner(true);
+    if (dirType === 'file') {
+      if (!folderId || !workspaceId) return;
+      dispatch({
+        type: 'UPDATE_FILE',
+        payload: { file: { bannerUrl: '' }, fileId, folderId, workspaceId },
+      });
+      await supabase.storage.from('file-banners').remove([`banner-${fileId}`]);
+      await updateFile({ bannerUrl: '' }, fileId);
+    }
+    if (dirType === 'folder') {
+      if (!workspaceId) return;
+      dispatch({
+        type: 'UPDATE_FOLDER',
+        payload: { folder: { bannerUrl: '' }, folderId: fileId, workspaceId },
+      });
+      await supabase.storage.from('file-banners').remove([`banner-${fileId}`]);
+      await updateFolder({ bannerUrl: '' }, fileId);
+    }
+    if (dirType === 'workspace') {
+      dispatch({
+        type: 'UPDATE_WORKSPACE',
+        payload: {
+          workspace: { bannerUrl: '' },
+          workspaceId: fileId,
+        },
+      });
+      await supabase.storage.from('file-banners').remove([`banner-${fileId}`]);
+      await updateWorkspace({ bannerUrl: '' }, fileId);
+    }
+    setDeletingBanner(false);
+  };
+
 
   const breadCrumbs = useMemo(() => {
     if (!pathname || !state.workspaces || !workspaceId) return;
@@ -270,18 +342,65 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ dirType, fileId, dirDetails }
           </div>
       </div>
     </div>
-    <div className="relative w-full h-[200px]">
     {details.bannerUrl && 
-      <Image
-        src={supabase.storage.from('file-banners').getPublicUrl(details.bannerUrl).data.publicUrl}
-        fill
-        className="w-full md:-48 h-20 object-cover "
-        alt="Banner Image"
-      />
+      <div className="relative w-full h-[200px]">
+        <Image
+          src={supabase.storage.from('file-banners').getPublicUrl(details.bannerUrl).data.publicUrl}
+          fill
+          className="w-full md:-48 h-20 object-cover "
+          alt="Banner Image"
+        />
+      </div>
     }
-      
-    </div>
     <div className="flex justify-center items-center flex-col mt-2 relative">
+      <div className="w-full self-center max-w-|800px] flex flex-col px-7 lg:my-8">
+        <div className="text-[80px]">
+          <EmojiPicker getValue={iconOnChange}>
+            <div className="w-[100px] cursor-pointer transition-colors h-[100px] flex items-center justify-center hover:bg-muted rounded-xl">
+              {details.iconId}
+            </div>
+          </EmojiPicker>
+        </div>
+        <div className="flex ">
+            <BannerUpload
+              id={fileId}
+              dirType={dirType}
+              className="mt-2
+              text-sm
+              text-muted-foreground
+              p-2
+              hover:text-card-foreground
+              transition-all
+              rounded-md"
+            >
+              {details.bannerUrl ? 'Update Banner' : 'Add Banner'}
+            </BannerUpload>
+            {details.bannerUrl && (
+              <Button
+                disabled={deletingBanner}
+                onClick={deleteBanner}
+                variant="ghost"
+                className="gap-2 hover:bg-background
+                flex
+                item-center
+                justify-center
+                mt-2
+                text-sm
+                text-muted-foreground
+                w-36
+                p-2
+                rounded-md"
+              >
+                <XCircleIcon size={16} />
+                <span className="whitespace-nowrap font-normal">
+                  Remove Banner
+                </span>
+              </Button>
+            )}
+          </div>
+
+
+      </div>
       <div id="container" ref={wrapperRef} className="max-w-[800px]"></div> 
     </div>
     </>
